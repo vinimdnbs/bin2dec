ebe2ba833098379a364221375448a6b7da04633b
Vinícius M. Dettogni <vinimdnbs@gmail.com>
2022-07-17 03:53:49 -0300

    tests

--- src/server.rb
+++ src/server.rb
@@ -2,17 +2,21 @@ require 'sinatra'
 
 get '/' do
   content_type 'text/html'
+
   send_file File.join(settings.public_folder, 'index.html')
 end
 
 post '/convert' do
   content_type 'application/json'
   data = JSON.parse request.body.read
-  decimalNumber = data['binaryNumber']
+
+  { 'decimalNumber' => bin2dec(data['binaryNumber']) }.to_json
+end
+
+def bin2dec(binaryNumber)
+  binaryNumber
     .reverse
     .split('')
     .each_with_index
     .reduce(0) { |memo, (element, index)| memo + (2 ** index) * element.to_i }
-    
-  { 'decimalNumber' => decimalNumber }.to_json
 end
---
+++ src/test.rb
@@ -0,0 +1,32 @@
+require 'rack/test'
+require 'test/unit'
+require_relative  'server'
+
+set :environment, :test
+
+class ServerTest < Test::Unit::TestCase
+  include Rack::Test::Methods
+
+  def app
+    Sinatra::Application
+  end
+
+  def test_response_is_ok
+    get '/'
+
+    assert last_response.ok?
+    assert last_response.body.include?('Bin2Dec')
+  end
+
+  def test_bin2dec
+    post '/convert', JSON.generate('binaryNumber' => '100'), 'CONTENT_TYPE' => 'application/json'
+    assert_equal 4, JSON.parse(last_response.body)['decimalNumber']
+
+    post '/convert', JSON.generate('binaryNumber' => '1000'), 'CONTENT_TYPE' => 'application/json'
+    assert_equal 8, JSON.parse(last_response.body)['decimalNumber']
+
+    post '/convert', JSON.generate('binaryNumber' => '10000001'), 'CONTENT_TYPE' => 'application/json'
+    assert_equal 129, JSON.parse(last_response.body)['decimalNumber']
+  end
+
+end
